7382a01a2c5b7bc6e72e5dc5f730f3866b5a0899
JanCesenek <jan.cesenekCZ@gmail.com>
2025-05-24 12:45:14 +0200

    copyright

--- src/pages/root.jsx
+++ src/pages/root.jsx
@@ -2,6 +2,8 @@ import React, { useContext } from "react";
 import { Outlet } from "react-router-dom";
 import { NotificationContext } from "../context/NotificationContext";
 import Notification from "../components/custom/notification";
+import { FaCopyright } from "react-icons/fa";
+import logo from "../imgs/flamebulb.svg";
 
 const RootLayout = () => {
   const { notification } = useContext(NotificationContext);
@@ -10,6 +12,14 @@ const RootLayout = () => {
     <>
       {notification && <Notification message={notification} />}
       <Outlet />
+      <div className="w-full h-[2rem] bg-black flex justify-center items-center text-[0.8rem] mt-20">
+        <FaCopyright className=" mr-2" />
+        <div className="flex items-center">
+          <p className="mr-2">|</p>
+          <img src={logo} alt="logo" className="w-[0.8rem]" />
+          <p className="ml-2">Jan Cesenek 2025 | All rights reserved</p>
+        </div>
+      </div>
     </>
   );
 };
